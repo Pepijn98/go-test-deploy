c590b4805b919257246dd2ade77c18c62eab0e8f
Pepijn98 <pepijn@vdbroek.dev>
2023-09-18 23:10:50 +0200

    Add greet route

--- main.go
+++ main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 )
 
-func RequestHandler(w http.ResponseWriter, r *http.Request) {
+func Index(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte("Hello World!"))
+	fmt.Fprintf(w, "Hello World!")
+}
+
+func Greet(w http.ResponseWriter, r *http.Request) {
+	query, err := url.ParseQuery(r.URL.RawQuery)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Bad Request")
+		return
+	}
+
+	name := query.Get("name")
+	if len(name) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Missing name parameter")
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hello %s", name)
 }
 
 func main() {
-	http.HandleFunc("/", RequestHandler)
+	http.HandleFunc("/", Index)
+	http.HandleFunc("/greet", Greet)
 
 	log.Fatal(http.ListenAndServe(":9999", nil))
 }
--- main_test.go
+++ main_test.go
@@ -7,17 +7,42 @@ import (
 	"testing"
 )
 
-func TestRequestHandler(t *testing.T) {
+func TestIndex(t *testing.T) {
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
 	w := httptest.NewRecorder()
-	RequestHandler(w, req)
+
+	Index(w, req)
+
 	res := w.Result()
+
 	defer res.Body.Close()
+
 	data, err := io.ReadAll(res.Body)
 	if err != nil {
 		t.Errorf("Error reading response body: %v", err)
 	}
+
 	if string(data) != "Hello World!" {
-		t.Errorf("Expected response body to be 'Hello World!', got '%s'", string(data))
+		t.Errorf("Expected 'Hello World!', got '%s'", string(data))
+	}
+}
+
+func TestGreet(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/greet?name=Pepijn", nil)
+	w := httptest.NewRecorder()
+
+	Greet(w, req)
+
+	res := w.Result()
+
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("Error reading response body: %v", err)
+	}
+
+	if string(data) != "Hello Pepijn" {
+		t.Errorf("Expected 'Hello Pepijn', got '%s'", string(data))
 	}
 }
